5751d308b44b9f4f256c6d1f3a96a8ca89ed8194
Ubuntumta <Ubuntumta@gmail.com>
2015-10-01 14:59:49 +0700

    Đã có giao diện Project 2 mọi người xem rồi cho ý kiến chỉnh sửa

---
+++ Project 2/QUANLY_HS_GV/QUANLY_HS_GV/GiaoVien.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUANLY_HS_GV
+{
+    public partial class GiaoVien : Form
+    {
+        public GiaoVien()
+        {
+            InitializeComponent();
+        }
+
+        private void GiaoVien_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'qUANLY_HS_GVDataSet1.GiaoVien' table. You can move, or remove it, as needed.
+            this.giaoVienTableAdapter.Fill(this.qUANLY_HS_GVDataSet1.GiaoVien);
+
+        }
+    }
+}
